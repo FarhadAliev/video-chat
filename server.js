63f11709625c1a7cbf4153962f5739aebd01bfb9
FarhadAliev <77681628+FarhadAliev@users.noreply.github.com>
2023-06-02 10:49:14 +0400

    code

--- server.js
+++ server.js
@@ -1,16 +1,8 @@
 const express = require('express')
 const app = express()
-// const cors = require('cors')
-// app.use(cors())
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
-const { ExpressPeerServer } = require('peer');
-const peerServer = ExpressPeerServer(server, {
-  debug: true
-});
 const { v4: uuidV4 } = require('uuid')
-
-app.use('/peerjs', peerServer);
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
@@ -26,17 +18,12 @@ app.get('/:room', (req, res) => {
 io.on('connection', socket => {
   socket.on('join-room', (roomId, userId) => {
     socket.join(roomId)
-    socket.to(roomId).emit('user-connected', userId);
-    // messages
-    socket.on('message', (message) => {
-      //send message to the same room
-      io.to(roomId).emit('createMessage', message)
-    });
+    socket.to(roomId).emit('user-connected', userId)
 
     socket.on('disconnect', () => {
-      socket.to(roomId).broadcast.emit('user-disconnected', userId)
+      socket.to(roomId).emit('user-disconnected', userId)
     })
   })
 })
 
-server.listen(3030)
+server.listen(3000)
